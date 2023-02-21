b71b6f3477d1173538c6ee97f4bd0d055a85ac21
Amogh Kapalli <amoghkapalli12@gmail.com>
2023-02-21 15:08:51 -0800

    Add files via upload

---
+++ make.sh
@@ -0,0 +1,5 @@
+# build
+javac -cp gson-2.8.6.jar *.java
+# to run, type in the following command with your own city name replacing <city name>:
+# java -cp .:gson-2.8.6.jar WeatherAPI <city name>
+# NOTE: this program only works for cities with one word. Cities like new dehli do not work
